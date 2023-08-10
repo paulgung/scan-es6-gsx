9e10257efddc81902dfd23b266ebd7f32c9f6baa
Paul Gung <gongshaoxu@gmail.com>
2023-08-10 10:00:46 +0800

    feat: updata

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const path = require('path');
 
 // 获取命令行参数
 const args = process.argv.slice(2);
-
+const cwd = process.cwd();
 // 引入主方法
 const scanFolderForES6Syntax = require('./scanFolder');
 
 // 读取命令行的指定path路径
 // 示例用法: node index.js 或者 node index.js dist 或者 node index.js --path=dist
-let folderPath = args[0] ? path.join(__dirname, args[0]) : path.join(__dirname, './dist');
+let folderPath = args[0] ? path.join(cwd, args[0]) : path.join(cwd, './dist');
 args.forEach(arg => {
     if (arg.startsWith('--path=')) {
         folderPath = path.join(__dirname, arg.substring(7));

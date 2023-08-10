79f94a106a26305e042ab2f04ba0048b0ed6fbb7
Paul Gung <gongshaoxu@gmail.com>
2023-08-10 09:57:56 +0800

    feat: scan es6 code

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+// 引入内置的 `process` 模块
+const process = require('process');
+const path = require('path');
+
+// 获取命令行参数
+const args = process.argv.slice(2);
+
+// 引入主方法
+const scanFolderForES6Syntax = require('./scanFolder');
+
+// 读取命令行的指定path路径
+// 示例用法: node index.js 或者 node index.js dist 或者 node index.js --path=dist
+let folderPath = args[0] ? path.join(__dirname, args[0]) : path.join(__dirname, './dist');
+args.forEach(arg => {
+    if (arg.startsWith('--path=')) {
+        folderPath = path.join(__dirname, arg.substring(7));
+    }
+});
+
+scanFolderForES6Syntax(folderPath);
---
+++ scanFolder.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const hasES6Syntax = require('./scanCode');
+
+// 扫描文件夹中的所有 JavaScript 文件并检查是否包含 ES6 语法
+function scanFolderForES6Syntax(folderPath) {
+    const files = fs.readdirSync(folderPath);
+
+    for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isDirectory()) {
+            // 递归扫描子文件夹
+            scanFolderForES6Syntax(filePath);
+        } else if (stats.isFile() && file.endsWith('.js')) {
+            try {
+                // 读代码
+                const code = fs.readFileSync(filePath, 'utf-8');
+                // 扫描单文件代码
+                const hasES6 = hasES6Syntax(code);
+                if (hasES6) {
+                    console.log(`文件 ${filePath} 中包含 ES6 语法`);
+                } else {
+                    console.log(`文件 ${filePath} 不包含 ES6 语法`);
+                }
+            } catch (error) {
+                console.error(`读取文件 ${filePath} 失败：${error}`);
+            }
+        }
+    }
+}
+
+module.exports = scanFolderForES6Syntax;
